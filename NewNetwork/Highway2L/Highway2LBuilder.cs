d3c4247fc50f5bcca8ea6537441eb9f9c1966469
davidcharlebois6 <david_charlebois@msn.com>
2015-08-08 02:15:41 -0400

    2LHighway lane correction

--- NewNetwork/Highway2L/Highway2LBuilder.cs
+++ NewNetwork/Highway2L/Highway2LBuilder.cs
@@ -58,7 +58,7 @@ namespace NetworkExtensions.NewNetwork.Highway2L
                             @"NewNetwork\Highway2L\Textures\Ground_Segment__AlphaMap.png"));
                     info.SetNodesTexture(
                         new TexturesSet
-                           (@"NewNetwork\Highway2L\Textures\Ground_Node__MainTex.png",
+                           (@"NewNetwork\Highway2L\Textures\Ground_Segment__MainTex.png",
                             null,
                             @"NewNetwork\Highway2L\Textures\Ground_Node__AlphaMap.png"),
                         new TexturesSet
