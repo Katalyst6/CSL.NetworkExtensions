91c42df9cb4270844e3cab8b082c0394525ddc47
davidcharlebois6 <david_charlebois@msn.com>
2015-08-12 19:18:11 -0400

    There is nothing good in the SomeRoad mod

--- Mod.cs
+++ Mod.cs
@@ -2,20 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using ColossalFramework.IO;
 using ColossalFramework.Steamworks;
 using ICities;
 using NetworkExtensions.Framework;
-using UnityEngine;
-using Object = UnityEngine.Object;
-#if DEBUG
-using Debug = NetworkExtensions.Framework.Debug;
-#endif
 
 namespace NetworkExtensions
 {
-    public class Mod : LoadingExtensionBase, IUserMod
+    public partial class Mod : IUserMod
     {
         private const UInt64 WORKSHOP_ID = 478820060;
 
@@ -33,73 +27,6 @@ namespace NetworkExtensions
         public string Description
         {
             get { return "An addition of highways and roads"; }
-        }
-
-        private bool _isReleased = true;
-        private GameObject _container = null;
-        private NetCollection _newRoads = null;
-        private ModInitializer _initalizer = null;
-
-        public override void OnCreated(ILoading loading)
-        {
-            base.OnCreated(loading);
-
-            if (_isReleased)
-            {
-                _container = new GameObject(NEXT_OBJECT_NAME);
-
-                _newRoads = _container.AddComponent<NetCollection>();
-                _newRoads.name = NEWROADS_NETCOLLECTION;
-
-                _initalizer = _container.AddComponent<ModInitializer>();
-                _initalizer.NewRoads = _newRoads;
-                _initalizer.InitializationCompleted += InitializationCompleted;
-
-                _isReleased = false;
-            }
-        }
-
-        private void InitializationCompleted(object sender, EventArgs e)
-        {
-            Loading.QueueAction(() =>
-            {
-                if (_initalizer != null)
-                {
-                    _initalizer.NewRoads = null;
-                    Object.Destroy(_initalizer);
-                    _initalizer = null;
-                }
-            });
-        }
-
-        public override void OnReleased()
-        {
-            base.OnReleased();
-
-            if (_isReleased)
-            {
-                return;
-            }
-
-            if (_initalizer != null)
-            {
-                Object.Destroy(_initalizer);
-                _initalizer = null;
-            }
-
-            if (_newRoads != null)
-            {
-                Object.Destroy(_newRoads);
-                _newRoads = null;
-            }
-
-            if (_container != null)
-            {
-                Object.Destroy(_container);
-                _container = null;
-            }
-
-            _isReleased = true;
         }
 
         public static string GetPath()
