856ceef65cee69ee8db54a288d6a5134a99fb660
andreharv <andreharv@yahoo.com>
2015-08-23 20:07:24 -0400

    Need to revert this

---
+++ NewNetwork/OneWay1L/Meshes/OneWay1LSegmentModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkExtensions.NewNetwork.OneWay1L.Meshes
+{
+    class OneWay1LSegmentModel
+    {
+    }
+}
---
+++ NewNetwork/OneWay1L/OneWAy1LBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkExtensions.NewNetwork.OneWay1L.Meshes;
+
+namespace NetworkExtensions.NewNetwork.OneWay1L
+{
+    class OneWay1LBuilder
+    {
+    }
+}
