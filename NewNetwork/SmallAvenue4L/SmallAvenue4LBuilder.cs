350915bcb8b34f92c699f985da986077f4341a83
davidcharlebois6 <david_charlebois@msn.com>
2015-08-24 22:24:04 -0400

    MediumAvenue with TL first draft

--- NewNetwork/SmallAvenue4L/SmallAvenue4LBuilder.cs
+++ NewNetwork/SmallAvenue4L/SmallAvenue4LBuilder.cs
@@ -7,7 +7,7 @@ namespace NetworkExtensions.NewNetwork.SmallAvenue4L
     public class SmallAvenue4LBuilder : ModPart, INetInfoBuilder
     {
         public int OptionsPriority { get { return 1; } }
-        public int Priority { get { return 5; } }
+        public int Priority { get { return 4; } }
 
         public string PrefabName { get { return "Basic Road"; } }
         public string Name { get { return "Small Avenue"; } }
