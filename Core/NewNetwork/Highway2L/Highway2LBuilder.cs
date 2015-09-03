492bcbbd6ee621c8a19cf3344f8ff94cf0ae19c2
davidcharlebois6 <david_charlebois@msn.com>
2015-09-03 07:59:27 -0400

    HW Mesh Lod fixing FOREVER!

--- Core/NewNetwork/Highway2L/Highway2LBuilder.cs
+++ Core/NewNetwork/Highway2L/Highway2LBuilder.cs
@@ -82,11 +82,11 @@ namespace NetworkExtensions.NewNetwork.Highway2L
 
                 nodes0.SetMeshes
                     (@"NewNetwork\Highway2L\Meshes\Ground.obj",
-                     @"NewNetwork\Highway2L\Meshes\Ground_HwTrans_LOD.obj");
+                     @"NewNetwork\Highway2L\Meshes\Ground_Node_LOD.obj");
 
                 nodes1.SetMeshes
                     (@"NewNetwork\Highway2L\Meshes\Ground_Trans.obj",
-                     @"NewNetwork\Highway2L\Meshes\Ground_RdTrans_LOD.obj");
+                     @"NewNetwork\Highway2L\Meshes\Ground_Trans_LOD.obj");
 
                 info.m_segments = new[] { segments0 };
                 info.m_nodes = new[] { nodes0, nodes1 };
