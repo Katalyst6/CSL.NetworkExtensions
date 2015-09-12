8d32c27d3901816cc3c4cc99ba7f6c24b3774f69
davidcharlebois6 <david_charlebois@msn.com>
2015-09-12 15:31:01 -0400

    Preventing ItemClass duplication

--- Core/Framework/Extensions/ItemClassExtensions.cs
+++ Core/Framework/Extensions/ItemClassExtensions.cs
@@ -8,17 +8,24 @@ namespace NetworkExtensions.Framework
 {
     public static class ItemClassExtensions
     {
+        private static readonly IDictionary<string, ItemClass> s_newClasses = new Dictionary<string, ItemClass>(StringComparer.InvariantCultureIgnoreCase); 
+
         public static ItemClass Clone(this ItemClass itemClass, string newName)
         {
-            var newClass = ScriptableObject.CreateInstance<ItemClass>();
-            newClass.m_layer = itemClass.m_layer;
-            newClass.m_level = itemClass.m_level;
-            newClass.m_service = itemClass.m_service;
-            newClass.m_subService = itemClass.m_subService;
-            newClass.hideFlags = itemClass.hideFlags;
-            newClass.name = newName;
+            if (!s_newClasses.ContainsKey(newName))
+            {
+                var newClass = ScriptableObject.CreateInstance<ItemClass>();
+                newClass.m_layer = itemClass.m_layer;
+                newClass.m_level = itemClass.m_level;
+                newClass.m_service = itemClass.m_service;
+                newClass.m_subService = itemClass.m_subService;
+                newClass.hideFlags = itemClass.hideFlags;
+                newClass.name = newName;
 
-            return newClass;
+                s_newClasses[newName] = newClass;
+            }
+
+            return s_newClasses[newName];
         }
     }
 }
